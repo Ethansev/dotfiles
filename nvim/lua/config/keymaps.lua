9f8efde6f86ab873931eef9351de8d686467b019
Ethansev <ethanbutformal@gmail.com>
2026-07-23 00:27:43 +0000

    Linux updates: paplay notification fallback, gh credential helper, railway env, nvim Snacks picker migration

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,19 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set(
-    "n",
-    "<leader>sx",
-    require("telescope.builtin").resume,
-    { noremap = true, silent = true, desc = "Resume" }
-)
-
--- vim.keymap.set(
---     "n",
---     "<leader>ss",
---     require("telescope.builtin").grep_string,
---     { noremap = true, silent = true, desc = "Grep String" }
--- )
+vim.keymap.set("n", "<leader>sx", function()
+    Snacks.picker.resume()
+end, { noremap = true, silent = true, desc = "Resume" })
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Center screen on down" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Center screen on up" })
